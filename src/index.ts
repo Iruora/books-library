558a3eac5a28c3d8e55d1f77903c9cf39ea0bed4
Nidhal AROURI <arrouurii@gmail.com>
2019-06-24 14:51:12 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {BooksLibraryApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {BooksLibraryApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new BooksLibraryApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
